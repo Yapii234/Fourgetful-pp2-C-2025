ad327ae6da868e3f4305ebbedb65b9e00626aa1c
Muhammad Yafi <yafi.233040113@mail.unpas.ac.id>
2025-12-30 20:44:26 +0700

    Initial Commit: Project setup

---
+++ Fourgetful_PP2_C_2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fourgetful_pp2_c_2025;
+
+/**
+ *
+ * @author Muhammad Yafi
+ */
+public class Fourgetful_PP2_C_2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
